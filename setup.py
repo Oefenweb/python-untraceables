3a88edded46e686219a4a9cdd9348228baa0eec5
Mischa ter Smitten <mischa@tersmitten.nl>
2024-12-17 10:45:30 +0100

    Bump version: 1.5.1 → 1.5.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def readme():
 
 
 setup(name='untraceables',
-      version='1.5.1',
+      version='1.5.2',
       author='Mischa ter Smitten',
       author_email='mtersmitten@oefenweb.nl',
       maintainer='Mischa ter Smitten',
